eb8413e7629893b02ebb8fe93217ade2743e247c
Adam Hudson <adamthudson@gmail.com>
2019-10-22 23:24:36 -0400

    .

--- runMinecraftServer.sh
+++ runMinecraftServer.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-cd /opt/minecraft-worlds/zuriel
 git pull
-java -Xmx1024M -Xms1024M -jar /opt/minecraft-server/server.jar nogui
+java -Xmx1024M -Xms1024M -jar server.jar nogui
 git add -A
 git commit -m "File push ${date} "
